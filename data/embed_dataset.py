a5605b304738313f3bf3320016959242b6c5e9eb
Deepesh Hada <deepesh.hada@gmail.com>
2021-06-12 16:07:20 +0530

    added dataset preprocessing files

---
+++ data/embed_dataset.py
@@ -0,0 +1,40 @@
+import pickle
+import itertools
+import pdb
+from time import time
+from collections import defaultdict
+import numpy as np
+import scipy.sparse as sp
+
+from data.dataset import Dataset
+from data.tenet_dataset import TenetDataset
+from utils import utils
+import torch
+
+
+class EmbedDataset(TenetDataset):
+    def __init__(self, args):
+        TenetDataset.__init__(self,args)
+        path = args.path+args.dataset
+        cosine = args.cosine == 'True'
+        print('Inside Embed Dataset')
+
+        '''
+        self.user_embed = torch.from_numpy(utils.load_npy(path + '.user_embed.npy')).to(args.device)
+        self.list_embed = torch.from_numpy(utils.load_npy(path + '.list_embed.npy')).to(args.device)
+        self.item_embed = torch.from_numpy(utils.load_npy(path + '.item_embed.npy')).to(args.device)
+        #pdb.set_trace()
+
+        #batch = torch.tensor([0, 0, 0, 0])
+        #edge_index = knn_graph(x, k=3, batch=batch, loop=False)
+        print("Cosine:",cosine)
+        self.user_edge_index = knn_graph(self.user_embed, k=args.knn_k, batch=None, loop=False,cosine=cosine)
+        self.list_edge_index = knn_graph(self.list_embed, k=args.knn_k, batch=None, loop=False,cosine=cosine)
+        self.item_edge_index = knn_graph(self.item_embed, k=args.knn_k, batch=None, loop=False,cosine=cosine)
+        '''
+
+        if cosine == False:
+            self.user_edge_index, self.list_edge_index, self.item_edge_index = utils.load_pickle(args.path + '/' + args.embed_type + '/' + str(args.knn_k) + '/' + args.dataset + '.user_list_item_knn.pkl')
+        else:
+            self.user_edge_index, self.list_edge_index, self.item_edge_index = utils.load_pickle(args.path + '/' + args.embed_type + '/' + str(args.knn_k) + '_cosine/' + args.dataset + '.user_list_item_knn.pkl')
+        # ==========================================
--- data/tenet_dataset.py
+++ data/tenet_dataset.py
@@ -1,96 +1,91 @@
+import pickle
+import itertools
+import pdb
+from time import time
 from collections import defaultdict
-
 import numpy as np
 import scipy.sparse as sp
+
 from data.dataset import Dataset
+from utils import utils
 
 
 class TenetDataset(Dataset):
-    def __init__(self, args):
-        Dataset.__init__(self, args)
+    def __init__(self,args):
+        Dataset.__init__(self,args)
+        #pdb.set_trace()
 
-        self.user_items_dct = self.get_user_items_dict(self.user_lists_dct, self.list_items_dct)
-        self.user_item_matrix_sp = self.get_sparse_matrix_from_dict(self.user_items_dct, self.num_user, self.num_item)
-        self.list_item_matrix_sp = self.get_sparse_matrix_from_dict(self.list_items_dct, self.num_list, self.num_item)
-        self.item_list_matrix_sp = self.get_sparse_matrix_from_dict(self.list_items_dct, self.num_item, self.num_list,
-                                                                    reverse=True)
+        self.user_items_dct          = self.get_user_items_dict(self.user_lists_dct, self.list_items_dct)
+        self.user_item_matrix_sp     = self.get_sparse_matrix_from_dict(self.user_items_dct, self.num_user, self.num_item)
+        self.list_item_matrix_sp     = self.get_sparse_matrix_from_dict(self.list_items_dct, self.num_list, self.num_item)
+        self.item_list_matrix_sp     = self.get_sparse_matrix_from_dict(self.list_items_dct, self.num_item, self.num_list, reverse=True)
 
-        self.user_user_comm_mat_sp = self.mat_mult_sp(self.user_item_matrix_sp,
-                                                      self.user_item_matrix_sp.T)
-        self.item_item_comm_mat_sp = self.mat_mult_sp(self.item_list_matrix_sp,
-                                                      self.item_list_matrix_sp.T)
-        self.list_list_comm_mat_sp = self.mat_mult_sp(self.list_item_matrix_sp,
-                                                      self.list_item_matrix_sp.T)
+        t1                   = time()
+        self.user_user_comm_mat_sp   = self.mat_mult_sp(self.user_item_matrix_sp, self.user_item_matrix_sp.T)#.astype(bool).astype(int)#todok()
+        self.item_item_comm_mat_sp   = self.mat_mult_sp(self.item_list_matrix_sp, self.item_list_matrix_sp.T)#.astype(bool).astype(int)
+        self.list_list_comm_mat_sp   = self.mat_mult_sp(self.list_item_matrix_sp, self.list_item_matrix_sp.T)##.astype(bool).astype(int) ##real values
+        print("""[%.2f s] """ %(time()-t1))
+        #pdb.set_trace()
 
+        # ==============================
+        #self.list_item_train_seq     = self.get_dct_mat_seq(dct=self.list_items_dct, num_row=self.num_list, num_col=self.num_item, padding_value=0)
+
+        ##self.train_matrix_item_seq   = self.get_dct_mat_seq_remove_test(dct=self.list_items_dct, num_row=self.num_list, num_col=self.max_item_seq_length+1, padding_value=0) ##last_index :] for all, :-1] for remove test item, :-2] for removing test and valid
+
+        #pdb.set_trace()
+
+        ##
+        '''
+        binarize = True
+        print("hello")
+        if binarize == True:
+            self.user_user_comm_mat_sp   = sp.csr_matrix(sp.csr_matrix((self.user_user_comm_mat_sp),dtype=bool),dtype=int)
+            self.item_item_comm_mat_sp   = sp.csr_matrix(sp.csr_matrix((self.item_item_comm_mat_sp),dtype=bool),dtype=int)
+            self.list_list_comm_mat_sp   = sp.csr_matrix(sp.csr_matrix((self.list_list_comm_mat_sp),dtype=bool),dtype=int)
+            #self.user_user_comm_mat_sp   = self.binarize_sparse_matrix(self.user_user_comm_mat_sp)
+            #self.item_item_comm_mat_sp   = self.binarize_sparse_matrix(self.item_item_comm_mat_sp)
+            #self.list_list_comm_mat_sp   = self.binarize_sparse_matrix(self.list_list_comm_mat_sp)
+
+        print("hello 2")
+        '''
+
+        # adj ===========
         self.user_adj_mat = self.user_user_comm_mat_sp
         self.list_adj_mat = self.list_list_comm_mat_sp
         self.item_adj_mat = self.item_item_comm_mat_sp
 
-        cosine = args.cosine == 'True'
+        print("hello")
 
-        # Embed dataset
-        if not cosine:
-            self.user_edge_index, self.list_edge_index, self.item_edge_index = utils.load_pickle(
-                args.path + '/' + args.embed_type + '/' + str(
-                    args.knn_k) + '/' + args.dataset + '.user_list_item_knn.pkl')
-        else:
-            self.user_edge_index, self.list_edge_index, self.item_edge_index = utils.load_pickle(
-                args.path + '/' + args.embed_type + '/' + str(
-                    args.knn_k) + '_cosine/' + args.dataset + '.user_list_item_knn.pkl')
-
+    # ==========================================
     def mat_mult_sp(self, mat1, mat2):
-        return mat1 * mat2
+        return mat1 * mat2 #.todtype(fset=int)
 
     def get_sparse_matrix_from_dict(self, dct, num_row, num_col, reverse=False):
-        sp_mat = sp.lil_matrix((num_row, num_col))
+        sp_mat = sp.lil_matrix((num_row,num_col))
 
         for key in dct:
             values = dct[key]
             for value in values:
-                if not reverse:
-                    sp_mat[key, value] = 1
+                if reverse == False:
+                    sp_mat[key,value] = 1
                 else:
-                    sp_mat[value, key] = 1
+                    sp_mat[value,key] = 1
         return sp_mat
 
     def get_user_items_dict(self, user_lists_dct, list_items_dct):
         user_items_dct = defaultdict(set)
         for user in user_lists_dct:
             for lst in user_lists_dct[user]:
+                #pdb.set_trace()
+                # for binary. if freq is wanted, change this to list and count during the sp_matrix conv (first binarize)
+                #print(set(list_items_dct[lst]))
                 user_items_dct[user] = user_items_dct[user].union(set(list_items_dct[lst]))
 
         return user_items_dct
 
+    def binarize_sparse_matrix(self, mat_sp):
+        bin_mat_sp = sp.lil_matrix(mat_sp.shape, dtype=np.float32)
+        for key in mat_sp.keys():
+            bin_mat_sp[key[0],key[1]] = 1
+        return bin_mat_sp
 
-class Batch(object):
-    def __init__(self, num_instances, batch_size, shuffle=True):
-        self.num_instances = num_instances
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.start = 0
-        self.epoch_completed = False
-        self.indices = np.arange(0, num_instances)
-        self.initialize_epoch()
-
-    def initialize_epoch(self):
-        self.initialize_next_epoch()
-
-    def initialize_next_epoch(self):
-        self.epoch_completed = False
-        self.start = 0
-        if self.shuffle:
-            np.random.shuffle(self.indices)
-
-    def get_next_batch_indices(self):
-        start = self.start
-        batch_size = self.batch_size
-        if start + batch_size < (self.num_instances - 1):
-            end = start + batch_size
-            self.start = end
-        else:
-            end = self.num_instances
-            self.epoch_completed = True
-        return self.indices[start:end]
-
-    def has_next_batch(self):
-        return not self.epoch_completed
